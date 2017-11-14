a9d14c0bef36180125d4e1ee409380b75f0411b3
georgezoto <gezotos@gmail.com>
2017-11-13 23:01:45 -0500

    Refactor fill-in-the-blanks.py based on review feedback.

--- Code Your Own Quiz/fill-in-the-blanks.py
+++ Code Your Own Quiz/fill-in-the-blanks.py
@@ -30,32 +30,33 @@
 
 #Let's begin :)
 #Data structrures our program will use
-#original_paragraph will store our original paragraph questions, one for each level of difficulty.
-original_paragraph = [[],[],[]]
 
 #easy level
-original_paragraph[0] = '''A common first thing to do in a language is to display
-'Hello __1__!'  In __2__ this is particularly easy; all you have to do
+easy_paragraph = '''A common first thing to do in a language is to display
+'Hello __0__!'  In __1__ this is particularly easy; all you have to do
 is type in:
-__3__ "Hello __1__!"
+__2__ "Hello __0__!"
 Of course, that isn't a very useful thing to do. However, it is an
-example of how to output to the user using the __3__ command, and
+example of how to output to the user using the __2__ command, and
 produces a program which does something, so it is useful in that capacity.
 
-You can also read input from a user using the __4__ command.
-This very program utilizes both commands written in __2__ to interact with you.'''
+You can also read input from a user using the __3__ command.
+This very program utilizes both commands written in __1__ to interact with you.'''
 
 #medium level
-original_paragraph[1] = '''A __1__ is created with the def keyword. You specify the inputs a __1__ takes by
-adding __2__ separated by commas between the parentheses. __1__s by default return __3__ if you
-don't specify the value to return. __2__ can be standard data types such as string, number, dictionary,
-tuple, and __4__ or can be more complicated such as objects and lambda functions.'''
+medium_paragraph = '''A __0__ is created with the def keyword. You specify the inputs a __0__ takes by
+adding __1__ separated by commas between the parentheses. __0__s by default return __2__ if you
+don't specify the value to return. __1__ can be standard data types such as string, number, dictionary,
+tuple, and __3__ or can be more complicated such as objects and lambda functions.'''
 
 #hard level
-original_paragraph[2] = '''According to the recent Paradise Papers, __1__, __2__, __3__, __4__,
+hard_paragraph = '''According to the recent Paradise Papers, __0__, __1__, __2__, __3__,
 Nike, Walmart, Allianz, Siemens, McDonald's, and Yahoo! are among the corporations that own offshore companies,
 as well as Allergan, the manufacturer of Botox. According to The Express Tribune, "Apple, Nike, and Facebook
 avoided billions of dollars in tax using offshore companies." Source: https://en.wikipedia.org/wiki/Paradise_Papers'''
+
+#original_paragraph dictionary will store our original paragraph questions, one for each level of difficulty.
+original_paragraph = {'easy': easy_paragraph, 'medium': medium_paragraph, 'hard': hard_paragraph}
 
 #The answers to the original_paragraphs above, one for each level of difficulty.
 #easy level
@@ -64,7 +65,9 @@ easy_answers = ['World', 'Python', 'print', 'raw_input']
 medium_answers = ['function', 'arguments', 'None', 'list']
 #hard level
 hard_answers = ['Facebook', 'Apple', 'Disney', 'Uber']
-answers = [easy_answers, medium_answers, hard_answers]
+answers = {'easy': easy_answers, 'easy_min_length': 0, 'easy_max_length': len(easy_answers),
+           'medium': medium_answers, 'medium_min_length': 0, 'medium_max_length': len(medium_answers),
+           'hard': hard_answers, 'hard_min_length': 0, 'hard_max_length': len(hard_answers)}
 
 #Conceptually different steps, broken down into seperate functions
 #Use Google format for Python doctstrings
@@ -104,13 +107,14 @@ def prompt_user_number_of_trials():
     Raises:
         None
     '''
-    guesses = ''
-    while guesses not in [1,2,3,4,5]:
+    print 'How many chances would you like to try, up to 5 per problem:'
+    guesses = raw_input()
+    while guesses not in ['1','2','3','4','5']:
         print 'How many chances would you like to try, up to 5 per problem:'
-        guesses = int(raw_input())
+        guesses = raw_input()
 
     print "Great! Let's start with " + str(guesses) + " guesses per problem."
-    return guesses
+    return int(guesses)
 
 def ask_user_paragraph(difficulty_level, guesses):
     '''
@@ -127,17 +131,58 @@ def ask_user_paragraph(difficulty_level, guesses):
         None
     '''
 
-    if difficulty_level == 'easy':
-        level = 0
-    elif difficulty_level == 'medium':
-        level = 1
-    else:
-        level = 2
+    if difficulty_level in ['easy', 'medium', 'hard']:
+        paragraph = original_paragraph[difficulty_level]
 
-    paragraph = original_paragraph[level]
-    print 'The current paragraph reads as such:'
+    print '\nThe current paragraph reads as such:'
     print paragraph
-    return [paragraph, 1]
+    initial_blank_id = 0
+    return [paragraph, initial_blank_id]
+
+def update_paragraph(paragraph, blank_id, answer):
+    '''
+    Update a given paragraph with the answer for the blank provided.
+
+    Args:
+        param1: paragraph
+        param2: blank_id
+        param3: answer
+
+    Returns:
+        Return updated paragraph and blan_id.
+
+    Raises:
+        None
+    '''
+
+    paragraph = paragraph.replace('__' + str(blank_id) + '__', answer)
+    print '\nThe current paragraph reads as such:'
+    print paragraph
+    blank_id += 1
+    return [paragraph, blank_id]
+
+def update_guesses(guesses):
+    '''
+    Update the number of guesses in case of an incorrect answer.
+
+    Args:
+        param1: guesses
+
+    Returns:
+        The updated number of guesses.
+
+    Raises:
+        None
+    '''
+
+    guesses -= 1
+    if guesses > 1:
+        print "That isn't the correct answer!  Let's try again; you have " + str(guesses) + " trys left!"
+    elif guesses == 1:
+        print "That isn't the correct answer! You only have " + str(guesses) + " try left! Make it count!"
+    else:
+        print "You've failed too many straight guesses!  Game over!"
+    return guesses
 
 def verify_answer_update_paragraph(difficulty_level, paragraph, blank_id, guesses):
     '''
@@ -149,39 +194,27 @@ def verify_answer_update_paragraph(difficulty_level, paragraph, blank_id, guesse
         param3: answer
 
     Returns:
-        Verify an answer and return updated paragraph.
+        Verify an answer and return the updated paragraph.
 
     Raises:
         None
     '''
 
-    while guesses > 0 and blank_id > 0 and blank_id < 5:
-        print 'What should be substituted in for __' + str(blank_id) + '__?'
-        answer = raw_input()
+    if difficulty_level in ['easy', 'medium', 'hard']:
+        difficulty_min_length = answers[difficulty_level + '_min_length']
+        difficulty_max_length = answers[difficulty_level + '_max_length']
 
-        if difficulty_level == 'easy':
-            level = 0
-        elif difficulty_level == 'medium':
-            level = 1
-        else:
-            level = 2
+        while guesses > 0 and blank_id >= difficulty_min_length and blank_id < difficulty_max_length:
+            print 'What should be substituted in for __' + str(blank_id) + '__?'
+            answer = raw_input()
 
-        if answers[level][blank_id - 1] == answer:
-            blank_id += 1
-            paragraph = paragraph.replace('__' + str(blank_id - 1) + '__', answer)
-            print 'The current paragraph reads as such:'
-            print paragraph
-        else:
-            guesses -= 1
-            if guesses > 1:
-                print "That isn't the correct answer!  Let's try again; you have " + str(guesses) + " trys left!"
-            elif guesses == 1:
-                print "That isn't the correct answer! You only have " + str(guesses) + " try left! Make it count!"
+            if answers[difficulty_level][blank_id] == answer:
+                [paragraph, blank_id] = update_paragraph(paragraph, blank_id, answer)
             else:
-                print "You've failed too many straight guesses!  Game over!"
+                guesses = update_guesses(guesses)
 
-        if blank_id == 5:
-            print '\n*** You won! ***'
+            if blank_id == difficulty_max_length:
+                print '\n*** You won! ***'
 
     return [paragraph, blank_id]
 
