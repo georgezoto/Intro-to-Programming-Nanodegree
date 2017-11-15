77336f243d3d45a6a439dcccbdf0d89d853a50a2
georgezoto <gezotos@gmail.com>
2017-11-14 20:13:21 -0500

    Update a basic drawing function.

--- Create a Movie Website/mindstorms.py
+++ Create a Movie Website/mindstorms.py
@@ -1,24 +1,35 @@
 import turtle
 
-def draw_square():
-    window = turtle.Screen()
-    window.bgcolor('red')
-
+def initialize_turtle(shape, color1, color2, speed):
     little_turtle = turtle.Turtle()
-    little_turtle.shape('turtle')
-    little_turtle.color("green", "yellow")
-    little_turtle.speed(2)
+    little_turtle.shape(shape)
+    little_turtle.color(color1, color2)
+    little_turtle.speed(speed)
+    return little_turtle
 
+def draw_square(little_turtle):
     for turn in range(0,4):
         little_turtle.forward(100)
         little_turtle.right(90)
 
-    small_turtle = turtle.Turtle()
-    small_turtle.shape('turtle')
-    small_turtle.color("white", "blue")
-    small_turtle.speed(2)
+def draw_triangle(turtle):
+    for turn in range(0,3):
+        turtle.forward(100)
+        turtle.right(120)
+
+def draw_circle_of_squares():
+    window = turtle.Screen()
+    window.bgcolor('red')
+
+    little_turtle = initialize_turtle('turtle', 'yellow', 'green', 0)
+
+    for turn in range(0,36):
+        draw_triangle(little_turtle)
+        little_turtle.right(10)
+
+    small_turtle = initialize_turtle('turtle', 'blue', 'white', 0)
     small_turtle.circle(100)
 
     window.exitonclick()
 
-draw_square()
+draw_circle_of_squares()
